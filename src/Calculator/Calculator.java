466ac4a67d6f23dafd7bcf48224df65f2ff95379
wildside <wildside@wildside-PC-New>
2020-03-26 14:38:20 +0900

    update

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -1,10 +1,25 @@
 package Calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("첫번째 값을 입력하세요 : ");
+		int first = sc.nextInt();
+		
+		System.out.println("두번째 값을 입력하세요 : ");
+		int second = sc.nextInt();
+		
+		sc.close();
+		
+		System.out.println(first + " + " + second + " = " + (first + second));
+		System.out.println(first + " - " + second + " = " + (first - second));
+		System.out.println(first + " * " + second + " = " + (first * second));
+		System.out.println(first + " / " + second + " = " + (first / second));
 		
 	}
 }

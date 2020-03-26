d75793987a8a437a46cadf79afbea75e61723b52
wildside <wildside@wildside-PC-New>
2020-03-26 15:17:26 +0900

    사칙연산 기호 입력

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -11,15 +11,26 @@ public class Calculator {
 		System.out.println("첫번째 값을 입력하세요 : ");
 		int first = sc.nextInt();
 		
+		System.out.println("연산 기호를 입력하세요. : ");
+		String symbol = sc.next();
+		
 		System.out.println("두번째 값을 입력하세요 : ");
 		int second = sc.nextInt();
 		
 		sc.close();
 		
-		System.out.println(first + " + " + second + " = " + (first + second));
-		System.out.println(first + " - " + second + " = " + (first - second));
-		System.out.println(first + " * " + second + " = " + (first * second));
-		System.out.println(first + " / " + second + " = " + (first / second));
+		if (symbol.equals("+")) {
+			System.out.println(first + " + " + second + " = " + (first + second));	
+		} else if (symbol.equals("-")) {
+			System.out.println(first + " - " + second + " = " + (first - second));	
+		} else if (symbol.equals("*")) {
+			System.out.println(first + " * " + second + " = " + (first * second));	
+		} else if (symbol.equals("/")) {
+			System.out.println(first + " / " + second + " = " + (first / second));	
+		} else {
+			System.out.println("사칙연산 기호가 아닙니다.");
+		}
+		
 		
 	}
 }

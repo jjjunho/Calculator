1327746672b4befe307704e60ace9faad69f3c2a
wildside <wildside@wildside-PC-New>
2020-03-26 16:29:36 +0900

    계산기 기본 기능 구현

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -11,26 +11,70 @@ public class Calculator {
 		System.out.println("첫번째 값을 입력하세요 : ");
 		int first = sc.nextInt();
 		
-		System.out.println("연산 기호를 입력하세요. : ");
-		String symbol = sc.next();
+		int result = 0;
+		int chkVal = 0;
+		String strResult = "";
 		
-		System.out.println("두번째 값을 입력하세요 : ");
-		int second = sc.nextInt();
-		
-		sc.close();
-		
-		if (symbol.equals("+")) {
-			System.out.println(first + " + " + second + " = " + (first + second));	
-		} else if (symbol.equals("-")) {
-			System.out.println(first + " - " + second + " = " + (first - second));	
-		} else if (symbol.equals("*")) {
-			System.out.println(first + " * " + second + " = " + (first * second));	
-		} else if (symbol.equals("/")) {
-			System.out.println(first + " / " + second + " = " + (first / second));	
-		} else {
-			System.out.println("사칙연산 기호가 아닙니다.");
+		while(true) {
+			
+			System.out.println("연산 기호를 입력하세요. : ");
+			String symbol = sc.next();
+			
+			if (symbol.equals("quit")) {
+				break;
+			}
+			
+			System.out.println("두번째 값을 입력하세요 : ");
+			int second = sc.nextInt();
+			
+//			sc.close();
+			
+			if (symbol.equals("+")) {
+//				System.out.println(first + " + " + second + " = " + (first + second));	
+				if (chkVal == 0) {
+					result = (first + second);
+					strResult = first + " + " + second;
+				} else {
+					result = (result + second);
+					strResult = strResult + " + " + second;
+				}
+			} else if (symbol.equals("-")) {
+//				System.out.println(first + " - " + second + " = " + (first - second));
+				if (chkVal == 0) {
+					result = (first - second);
+					strResult = first + " - " + second;
+				} else {
+					result = (result - second);
+					strResult = strResult + " - " + second;
+				}
+			} else if (symbol.equals("*")) {
+//				System.out.println(first + " * " + second + " = " + (first * second));
+				if (chkVal == 0) {
+					result = (first * second);
+					strResult = first + " * " + second;
+				} else {
+					result = (result * second);
+					strResult = strResult + " * " + second;
+				}
+			} else if (symbol.equals("/")) {
+//				System.out.println(first + " / " + second + " = " + (first / second));
+				if (chkVal == 0) {
+					result = (first / second);
+					strResult = first + " / " + second;
+				} else {
+					result = (result / second);
+					strResult = strResult + " / " + second;
+				}
+			} else {
+				System.out.println("사칙연산 기호가 아닙니다.");
+			}
+			
+			System.out.println(strResult + " = " + result);
+			System.out.println("------------------");
+			chkVal++;
 		}
 		
+		System.out.println(strResult + " = " + result);
 		
 	}
 }
